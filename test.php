d560d54b995e9d717940a31d885e6ac9af65a525
Joy Kim <jojo080889@gmail.com>
2012-05-11 15:39:44 -0700

    testing_tab: some styling of the rules div

--- test.php
+++ test.php
@@ -1,24 +1,27 @@
 <?php include "header.php" ?>
-	<div id="test_actions">
-		<input id=-"add_user" type="button" value="Add user/device" />
-		<input type="text" id="appId" value="game_template" />
-		<input id="app_launch" type="button" value="Launch"/>
-	</div>
-    	
-	<div id="rule_summary" class="sansa"> 
-		Game Logic
-		<p>
-		trigger 1<br/>
-		trigger 1<br/>
-		trigger 1<br/>
-		trigger 1<br/>
-		trigger 1<br/>
-		trigger 1<br/>
-		trigger 1<br/>
+	
+	<div id="rule_summary"> 
+		<h1>How to Play</h1>
+		<p class="info_text">
+			These rules have been automatically generated from the program you've created.
 		</p>
+		<ul id="rule_list">
+			<li>
+				On the start of your turn, roll a dice and move the number of spaces you roll.
+			</li>
+			<li>
+				When you land on the same square an opponent is occupying, the opponent must move their piece back to start.
+			</li>
+		</ul>
 	</div>
 
-	<div id="devices"></div>
-	
+	<div id="test_area">
+		<div id="test_actions">
+			<input id="add_user" type="button" value="Add user/device" />
+			<input type="text" id="appId" value="game_template" />
+			<input id="app_launch" type="button" value="Launch"/>
+		</div>
+		<div id="devices"></div>
+    </div>
 </body>
 </html>
