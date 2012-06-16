c83eb510b4e33140fbd500af7ac9eb91b2058606
Joy Kim <jojo080889@gmail.com>
2012-06-15 22:55:26 -0700

    master: remove experiments and extra files

--- joy_experiments/load.php
+++
@@ -1,6 +0,0 @@
-<?php
-	if ($_FILES['uploadedfile']['error'] == UPLOAD_ERR_OK 
-		  && is_uploaded_file($_FILES['uploadedfile']['tmp_name'])) { //checks that file is uploaded
-	  echo file_get_contents($_FILES['uploadedfile']['tmp_name']); 
-	}
-?>
